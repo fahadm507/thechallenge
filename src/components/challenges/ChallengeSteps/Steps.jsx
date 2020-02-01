96e21899768e026d3e87a1a05f9945bf17eaef40
Fahad Mukunzi <fahadm507@gmail.com>
2020-02-01 08:48:04 -0500

    Allow users to dynamically add steps during challenge creation.(WIp)

--- src/components/challenges/ChallengeSteps/Steps.jsx
+++ src/components/challenges/ChallengeSteps/Steps.jsx
@@ -1,33 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ChallengeForm from '../ChallengeForm';
-import { Steps, Divider, Input } from 'antd';
+import { Steps, Divider, Input, Tabs, Button } from 'antd';
 const { Step } = Steps;
 const { TextArea } = Input;
-
+const { TabPane } = Tabs;
 
 export const BasicInformation = props => {
     return <ChallengeForm { ...props}/>
 };
 
-export const ChallengeActionSteps = props => {
-    const {current} = props;
-    return (
-        <div>
-          <Divider />
-  
-          <Steps current={current} direction="vertical">
-            <Step title={<Input placeholder="Enter step 1" />} description="This is a description." />
-            <Step title={<TextArea placeholder="Enter description" />} description="This is a description." />
-            <Step title="Step 3" description="This is a description." />
-            <Step title={<Input />} description="This is a description." />
-            <Step title="Step 2" description="This is a description." />
-            <Step title="Step 3" description="This is a description." />
-            <Step title={<Input />} description="This is a description." />
-            <Step title="Step 2" description="This is a description." />
-            <Step title="Step 3" description="This is a description." />
-          </Steps>
-        </div>
-      );
+export class ChallengeActionSteps extends Component {
+    static defaultProps = {
+        initialActionStep: { 
+            id: 1,
+            title: '', 
+            description: '' 
+        }
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            actionSteps: [ { ...this.props.initialActionStep }]
+        };
+    }
+
+    addNewActionStep = () => {
+        const { actionSteps } = this.state;
+        const id = actionSteps.length + 1;
+        this.setState({
+            actionSteps: [
+                ...actionSteps,
+                { ...this.props.initialActionStep, id }
+            ] 
+        });
+    };
+
+    getActionSteps = () => {
+        const { actionSteps } = this.state;
+        const { current } = this.props;
+        return (
+            <Steps current={current} direction="vertical">
+                {actionSteps.map((action, actionIndex) => (
+                    <Step key={actionIndex}
+                        title={
+                            <Input 
+                                placeholder={`Enter step ${actionIndex + 1}`}
+                                value={action.description}
+                            />
+                        }
+                        description={
+                            <TextArea
+                                value={action.description}
+                                placeholder={`Enter description for step ${actionIndex + 1}`}
+                            />
+                        } 
+                    />
+                ))}
+            </Steps>
+        )
+    };
+
+    render(){
+        // const { challenge, form } = this.props;
+        return (
+            <div>
+                <Divider />
+                <div className="card-container">
+                    <Tabs type="card" size='all'>
+                        <TabPane tab="Action steps"
+                                 key="1"
+                                 tabPosition="right">
+                            {this.getActionSteps()}
+                            <Button type='default' onClick={this.addNewActionStep}>Add action</Button>
+                        </TabPane>
+                        <TabPane tab="Preview changes" key="2">
+                            Add preview of action steps here.
+                        </TabPane>
+                    </Tabs>
+                </div>   
+            </div>
+        );
+    }   
 };
 
 export const FinalStep = props => {
